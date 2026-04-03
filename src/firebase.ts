9766adc823e3b79541ef7363a5ff3bd313092d74
Justin King <kingjust@mail.gvsu.edu>
2026-04-03 00:24:06 +0000

    Switch firebase configuration to GVSU app
    - Accidentally setup original app with personal GMail account

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAjxLSg_gzVwATj_55a5JDjUE0adVtphyE",
-  authDomain: "beverageshop-2819b.firebaseapp.com",
-  projectId: "beverageshop-2819b",
-  storageBucket: "beverageshop-2819b.firebasestorage.app",
-  messagingSenderId: "567176661770",
-  appId: "1:567176661770:web:e188f56e378934bd3f08f3"
+  apiKey: "AIzaSyA6iipY6gKsubIC406hpuZkK_Fnh1VXohE",
+  authDomain: "beverageshop-11901.firebaseapp.com",
+  projectId: "beverageshop-11901",
+  storageBucket: "beverageshop-11901.firebasestorage.app",
+  messagingSenderId: "560584874575",
+  appId: "1:560584874575:web:d2b9e2dc65c71ad96c683a"
 };
 
 const app = initializeApp(firebaseConfig);
